8e2f17290af171eeaf7ad483262f493951648c1a
Loremxs <burkeev.valera03@mail.ru>
2025-08-23 01:35:21 +0300

    added searchQuerry and Login page form

---
+++ src/app/components/textField.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const TextField = ({ label, type = "text", name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const getInputClasses = () => {
+    return "form-control " + (error ? "is-invalid" : "");
+  };
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+  return (
+    <div className="mb-4">
+      <label htmlFor={name}>{label}</label>
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          value={value}
+          onChange={onChange}
+          name={name}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={toggleShowPassword}
+          >
+            <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error} </div>}
+      </div>
+    </div>
+  );
+};
+
+export default TextField;
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -7,12 +7,13 @@ import GroupList from "../components/groupList";
 import SearchStatus from "../components/searchStatus";
 import UserTable from "../components/usersTable";
 import UserPage from "../components/userPage";
-import _ from "lodash";
+import _, { set } from "lodash";
 const Users = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [professions, setProfession] = useState();
   const [selectedProf, setSelectedProf] = useState();
   const [sortBy, setSortBy] = useState({ path: "name", order: "asc" });
+  const [searchQuery, setSearchQuery] = useState("");
   const pageSize = 8;
 
   const [users, setUsers] = useState();
@@ -37,10 +38,15 @@ const Users = () => {
 
   useEffect(() => {
     setCurrentPage(1);
-  }, [selectedProf]);
+  }, [selectedProf, searchQuery]);
 
   const handleProfessionSelect = (item) => {
+    if (searchQuery !== "") setSearchQuery("");
     setSelectedProf(item);
+  };
+  const handleSearchQuery = ({ target }) => {
+    setSelectedProf(undefined);
+    setSearchQuery(target.value);
   };
 
   const handlePageChange = (pageIndex) => {
@@ -66,7 +72,11 @@ const Users = () => {
   }, [users]);
 
   if (users) {
-    const filteredUsers = selectedProf
+    const filteredUsers = searchQuery
+      ? users.filter((user) =>
+          user.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : selectedProf
       ? users.filter(
           (user) =>
             JSON.stringify(user.profession) === JSON.stringify(selectedProf)
@@ -90,13 +100,19 @@ const Users = () => {
               onItemSelect={handleProfessionSelect}
             />
             <button className="btn btn-secondary mt-2" onClick={clearFilter}>
-              {" "}
               Очистить
             </button>
           </div>
         )}
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={handleSearchQuery}
+          />
           {count > 0 && (
             <UserTable
               users={usersCrop}
